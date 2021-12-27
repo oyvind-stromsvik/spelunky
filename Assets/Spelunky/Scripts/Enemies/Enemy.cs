e8ee6c7c980ab24decde5f3fccafc9e34cfd6f9b
Øyvind Strømsvik <oyvind@nymedia.no>
2021-12-27 19:40:35 +0100

    Move the Die logic from the Enemy class to the EntityHealth class

--- Assets/Spelunky/Scripts/Enemies/Enemy.cs
+++ Assets/Spelunky/Scripts/Enemies/Enemy.cs
@@ -4,8 +4,6 @@ namespace Spelunky {
 
     [RequireComponent(typeof(EntityPhysics), typeof(EntityHealth), typeof(EntityVisuals))]
     public class Enemy : MonoBehaviour {
-        public GameObject bloodParticles;
-
         public EntityPhysics EntityPhysics { get; private set; }
         public EntityHealth EntityHealth { get; private set; }
         public EntityVisuals EntityVisuals { get; private set; }
@@ -14,20 +12,8 @@ namespace Spelunky {
             EntityPhysics = GetComponent<EntityPhysics>();
             EntityHealth = GetComponent<EntityHealth>();
             EntityVisuals = GetComponent<EntityVisuals>();
-
-            EntityHealth.HealthChangedEvent.AddListener(OnHealthChanged);
         }
 
-        public virtual void OnHealthChanged() {
-            if (EntityHealth.CurrentHealth <= 0) {
-                Die();
-            }
-        }
-
-        private void Die() {
-            Instantiate(bloodParticles, transform.position, Quaternion.identity);
-            Destroy(gameObject);
-        }
     }
 
 }
